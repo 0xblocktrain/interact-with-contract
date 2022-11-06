77b60b00e16325205b9cada67473ff6f0bf1e616
Deveshb15 <deveshb583@gmail.com>
2022-11-06 14:37:30 +0530

    chore: connect wallet

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,40 @@
+import { useState, useEffect } from 'react'
+import { ethers } from 'ethers'
+import { CONTRACT_ADDRESS, CONTRACT_ABI } from '../utils/contract'
 import Head from 'next/head'
 
 export default function Home() {
+
+  const [address, setAddress] = useState(null)
+  const [contract, setContract] = useState(null)
+  const [loading, setLoading] = useState(false)
+
+  const connectWallet = async () => {
+    if(window) {
+      const { ethereum } = window
+      if(ethereum) {
+        try {
+          const accounts = await ethereum.request({ method: "eth_requestAccounts" })
+          console.log(accounts)
+          setAddress(accounts[0])
+          let provider = new ethers.providers.Web3Provider(ethereum)
+          let signer = provider.getSigner()
+          let c = new ethers.Contract(CONTRACT_ADDRESS, CONTRACT_ABI, signer)
+          setContract(c)
+        } catch(err) {
+          console.log(err)
+        }
+      }
+    }
+  }
+
+  console.log("ADDRESS ", address)
+  console.log("CONTRACT ", contract)
+  
+  const mintNft = async() => {
+    console.log("Minted")
+  }
+
   return (
     <div>
       <Head>
@@ -9,8 +43,19 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex items-center justify-center min-h-screen">
-        <h1 className='text-4xl font-extrabold'>Hello world</h1>
+      <main className="flex flex-col gap-4 items-center justify-center min-h-screen">
+        <h1 className='text-4xl font-extrabold'>Interact with contract</h1>
+        {
+          loading ? (
+            <h1 className='text-2xl font-extrabold'>Loading...</h1>
+          ) : (
+            address ? (
+              <button onClick={mintNft} className='py-2 px-4 rounded-xl bg-white text-black transform hover:scale-105'>Mint NFT</button>
+              ) : (
+              <button onClick={connectWallet} className='py-2 px-4 rounded-xl bg-white text-black transform hover:scale-105'>Connect Wallet</button>
+            )
+          )
+        }
       </main>
     </div>
   )
